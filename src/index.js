7357d4b7894de69e640d917c79eb48026031eee7
Tymur Bondar <tymurbondar@outlook.com>
2023-09-26 10:20:19 -0400

    movie Rec working

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 const projectList = document.getElementById("projectLinks");
 
-function listProject(project){
+function listProject(project) {
     const newLi = document.createElement("li");
     const newA = document.createElement("a");
-    const newName = document.createElement("h2");
+    const newName = document.createElement("h5");
     const newDesc = document.createElement("p");
-    newDesc.textContent = project.description;
+    newDesc.textContent = project.technologies;
+    newDesc.style.fontSize = "1rem";
     newName.textContent = project.name;
-    newA.textContent = project.link;
-    newLi.append(newA);
+    newA.href = project.link;
+    newA.target = "_blank";
+    newA.append(newName);
+    newLi.append(newA, newDesc);
     document.getElementById("projectLinks").append(newLi);
 
 }
 
-fetch("http://localhost:3000/projects")
-.then(res => res.json())
-.then(projects => {
-    projects.forEach(project => {
-        
+if (document.getElementById("index")) {
+    document.addEventListener("keyup", (e) => {
+        if (e.key === "m" || e.key === "M") {
+            fetch("http://localhost:3000/movies")
+                .then(res => res.json())
+                .then(projects => {
+                    const movie = projects[(Math.floor(Math.random() * projects.length))];
+                    document.getElementById("movieRec").textContent = `Movie: ${movie.movie}, Director: ${movie.director}`;
+                })
+        }
     });
-})
+};
+
+if (document.getElementById("portfolioPage")) {
+    fetch("http://localhost:3000/projects")
+        .then(res => res.json())
+        .then(projects => {
+            projects.forEach(project => {
+                listProject(project);
+            });
+        })
+}
