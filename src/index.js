82028981d83b824a4a6fdfc5a9f4ebd5edb62e8e
Tymur <tymurbondar@outlook.com>
2023-09-26 07:54:06 -0400

    nights work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const projectList = document.getElementById("projectLinks");
 
 function listProject(project){
-    const newLi = document.createElement("li")
+    const newLi = document.createElement("li");
+    const newA = document.createElement("a");
+    const newName = document.createElement("h2");
+    const newDesc = document.createElement("p");
+    newDesc.textContent = project.description;
+    newName.textContent = project.name;
+    newA.textContent = project.link;
+    newLi.append(newA);
+    document.getElementById("projectLinks").append(newLi);
+
 }
 
 fetch("http://localhost:3000/projects")
