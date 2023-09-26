898e2c5c4b0aa16bf1653bf2e9fb16a7f8e8f1cd
Tymur Bondar <tymurbondar@outlook.com>
2023-09-26 10:36:48 -0400

    second event listener

--- src/index.js
+++ src/index.js
@@ -38,3 +38,19 @@ if (document.getElementById("portfolioPage")) {
             });
         })
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add click event listener to all links with 'transition-link' class
+    document.querySelectorAll('.transition-link').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent default link behavior
+
+            // Add 'fade-out' class to body to start the fade-out transition
+            document.body.classList.add('fade-out');
+            // When the transition ends, navigate to the link's href
+            setTimeout(() => {
+                window.location.href = link.href;
+            }, 500); // 500ms corresponds to the transition duration in the CSS
+        });
+    });
+});
