28ec0af61e2e5e932e7e601937355ebebae15de1
TymurBondar <tymurbondar@outlook.com>
2023-11-01 10:31:19 -0400

    add links to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,12 @@
 function Contact(){
     return(
         <div className="text-left text-3xl">
-        <p>This is my Contact component</p>
+        <ul>
+            <li><a href="https://github.com/TymurBondar" target="_blank">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/tymurbondar/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://t.me/BondarTymur" target="_blank">Telegram</a></li>
+            <li>tymurbondar@outlook.com</li>
+        </ul>
         </div>
     );
 };
